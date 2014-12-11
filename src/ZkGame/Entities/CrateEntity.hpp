62041eb3f3b18ceee48bb4de317f0039a2929217
Hubert Pełczyński <forkast@students.mimuw.edu.pl>
2014-12-11 16:23:11 +0100

    Changed header file names from .h to .hpp

---
+++ src/ZkGame/Entities/CrateEntity.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include "Entity.hpp"
+
+namespace Zk {
+namespace Game {
+
+class Renderable;
+
+class CrateEntity : public Entity
+{
+public:
+	CrateEntity(sf::Vector2f pos, sf::Vector2f size, const char * imgSrc);
+	virtual ~CrateEntity();
+
+	virtual void pickUp() = 0;
+};
+
+}}
